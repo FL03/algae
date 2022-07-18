ce6e79c341f9c076e816505c1104a635b1ba7d14
FL03 <jo3mccain@icloud.com>
2022-07-18 10:45:14 -0500

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- algae/src/data/structures/trees/merkle.rs
+++ algae/src/data/structures/trees/merkle.rs
@@ -6,9 +6,24 @@
        ... Summary ...
 */
 
+#[derive(Clone, Debug, Hash, PartialEq, serde::Deserialize, serde::Serialize)]
+struct Node<T> {
+    keys: Vec<T>,
+    children: Vec<Node<T>>,
+}
+
+impl<T> Node<T> where T: Ord {
+    fn constructor(keys: Vec<T>, children: Vec<Node<T>>) -> Self {
+        Self { keys, children }
+    }
+}
+
+#[derive(Clone, Debug, Hash, PartialEq, serde::Deserialize, serde::Serialize)]
+pub struct MerkleTree;
+
+
 #[cfg(test)]
 mod tests {
-
     #[test]
     fn simple() {
         let f = |x: usize| x.pow(x.try_into().unwrap());
