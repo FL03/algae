163da1e5a556e7a0da9ed77e76c9fcf85fad3fac
FL03 <jo3mccain@icloud.com>
2022-07-27 14:41:55 -0500

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- algae/src/actors/mod.rs
+++ algae/src/actors/mod.rs
@@ -1,12 +1,11 @@
 /*
-   Appellation: actors
-   Context:
+   Appellation: actors <module>
    Creator: FL03 <jo3mccain@icloud.com>
    Description:
        ... Summary ...
 */
-pub(crate) mod actor;
-pub(crate) mod coin_change;
-pub(crate) mod edit_distance;
+pub use coin_change::*;
+pub use edit_distance::*;
 
-pub use crate::actors::{actor::*, coin_change::*, edit_distance::*};
+mod coin_change;
+mod edit_distance;
