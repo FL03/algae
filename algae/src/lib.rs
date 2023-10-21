56b9678523a59d4ccff82dd81c1191ad40f29741
FL03 <jo3mccain@icloud.com>
2023-10-21 08:02:41 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- algae/src/lib.rs
+++ algae/src/lib.rs
@@ -7,8 +7,6 @@
 //! Algae is a comprehensive collection of algorithms and data-structures written in Rust.
 #[cfg(feature = "graph")]
 pub use algae_graph as graph;
-#[cfg(feature = "math")]
-pub use algae_math as math;
 #[cfg(feature = "merkle")]
 pub use algae_merkle as merkle;
 #[cfg(feature = "mmr")]
@@ -18,15 +16,13 @@ pub mod list;
 
 pub mod prelude {
     #[cfg(feature = "graph")]
-    pub use super::graph;
+    pub use algae_graph::graph;
     // pub use super::graph::*;
-    #[cfg(feature = "math")]
-    pub use algae_math::prelude::*;
     #[cfg(feature = "merkle")]
-    pub use super::merkle::prelude::*;
+    pub use algae_merkle::prelude::*;
     #[cfg(feature = "mmr")]
-    pub use super::mmr::*;
+    pub use algae_mmr::*;
 
-    pub use super::list::*;
+    pub use crate::list::*;
 
 }
